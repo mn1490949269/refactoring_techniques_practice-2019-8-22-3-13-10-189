742ff94b9b70f2f3cced93d9e49f13c7f6be10cd
mn <1490949269@qq.com>
2019-08-27 15:01:15 +0800

    1

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -6,26 +6,39 @@ import java.util.List;
 import java.util.Vector;
 
 public class OwingPrinter {
-    void printOwing(String name, List<Order> orders) {
+	void printOwing(String name,List<Order> orders) {
         Iterator<Order> elements = orders.iterator();
         double outstanding = 0.0;
-
         // print banner
-        System.out.println ("*****************************");
-        System.out.println ("****** Customer totals ******");
-        System.out.println ("*****************************");
+        banner();
 
         // print owings
-        while (elements.hasNext()) {
+        outstanding = printOwings(elements, outstanding);
+
+        // print details
+        printDetailes(name,outstanding);
+    }
+	public double printOwings(Iterator<Order> elements, double outstanding) {
+		while (elements.hasNext()) {
             Order each = (Order) elements.next();
             outstanding += each.getAmount();
         }
-
-        // print details
-        System.out.println("name: " + name);
+		return outstanding;
+	}
+	public void printDetailes(String name,double outstanding) {
+		System.out.println("name: " + name);
         System.out.println("amount: " + outstanding);
+	}
+    void printString(String name, double outstanding) {
+    	
     }
+	public void banner() {
+		System.out.println ("*****************************");
+    	System.out.println ("****** Customer totals ******");
+    	System.out.println ("*****************************");
+	}
 }
+
 
 class Order {
     private final double amount;
--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -2,9 +2,15 @@ package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
     double getPrice(int quantity, int itemPrice) {
+    	double price;
         // Price consists of: base price - discount + shipping cost
-        return quantity * itemPrice -
-                Math.max(0, quantity - 500) * itemPrice * 0.05 +
-                Math.min(quantity * itemPrice * 0.1, 100.0);
+    	//base price
+    	double basePrice = quantity * itemPrice;
+    	//discount
+    	double discount = Math.max(0, quantity - 500) * itemPrice * 0.05;
+    	//shippingCost
+    	double shippingCost = Math.min(quantity * itemPrice * 0.1, 100.0);
+    	price = basePrice - discount + shippingCost;
+        return price;
     }
 }
